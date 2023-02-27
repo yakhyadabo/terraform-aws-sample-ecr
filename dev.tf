02bdbf64a231a4e371517b8cdbe7f8d9470f2720
Yakhya Dabo <yakhyadabo@gmail.com>
2023-02-27 01:48:27 +0000

    [dev] Add params

--- dev.tf
+++ dev.tf
@@ -1,9 +1,12 @@
 locals {
   repository_name = format("%s-%s", var.project.name, var.project.environment)
+  services = ["vault","cp-kafka","redis","nginx", "dashboard","grafana","prometheus"]
 }
 
 
 module "ecr" {
   source               = "./module"
   repository_name      = local.repository_name
+  services             = local.services
+  force_delete         = true
 }
